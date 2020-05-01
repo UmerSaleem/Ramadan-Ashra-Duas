fbd4fc9d294459905a36beafe6215d0a218e92d1
UmerSaleem <mr.umersaleem@gmail.com>
2020-05-01 09:02:28 +0800

    Commit for Version 1.0 for app "Ramadan Ashra Duas" applied on Google PLay for publishing.

---
+++ app/src/main/java/com/babli/ramadanashraduas/MainActivity.java
@@ -0,0 +1,27 @@
+package com.babli.ramadanashraduas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//Code to include Toolbar and its attributes
+   /*     Toolbar toolbar = findViewById(R.id.myToolBar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Ramadan Ashra Duas");
+    */
+
+
+    }
+}
